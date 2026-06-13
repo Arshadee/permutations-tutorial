2806a2e01c47b180d113fb477d5632ad01c89bad
ArshadMayet <arshadm@icetech.io>
2026-06-13 14:30:34 +0200

    Updated Main file to use UserInput as documented in README.md

--- src/main/java/org/calculate/permutate/Main.java
+++ src/main/java/org/calculate/permutate/Main.java
@@ -9,11 +9,11 @@ public class Main
 
     public static void main(String[] args) {
 
-              StaticInput staticInput = new StaticInput();
-              staticInput.run();
+//              StaticInput staticInput = new StaticInput();
+//              staticInput.run();
 
-//        UserInput userInput = new UserInput();
-//        userInput.run(args);
+        UserInput userInput = new UserInput();
+        userInput.run(args);
 
 
     }
